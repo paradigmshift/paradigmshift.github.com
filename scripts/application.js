f53e11974a49010fcde2c5d67ee80fa4c1678937
mozart reina <mozart@mozartreina.com>
2015-11-16 19:20:14 +0530

    fix styling issues due to change to Bootstrap

--- scripts/application.js
+++ scripts/application.js
@@ -7,4 +7,6 @@
 
 $(document).ready(function (){
     $('h3').prepend('<span class="glyphicon glyphicon-bookmark" aria-hidden="true"></span>');
+    $('span.display-data').wrap("<span style='font-size:1.2em'></span>")
+    $('span.highlight').wrap("<span class='bg-success' style='font-weight:900'></span>")
 });
