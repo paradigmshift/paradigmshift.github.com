06fbb7ca6e3e13ce9bd6fbe4391144823baf93a4
mozart reina <mozart@mozartreina.com>
2015-11-17 19:43:20 +0530

    Finish fixing styling issues

--- scripts/application.js
+++ scripts/application.js
@@ -1,12 +1,6 @@
-// $(document).ready(function (){
-//     $h3 = $('h3');
-//     $h3.each(function(index) {
-//         $(this).replaceWith("<div class='panel panel-default'> <div class='panel-heading section-header'>" + $(this).text() + "</div> </div>");
-//     });
-// });
-
 $(document).ready(function (){
     $('h3').prepend('<span class="glyphicon glyphicon-bookmark" aria-hidden="true"></span>');
-    $('span.display-data').wrap("<span style='font-size:1.2em'></span>")
-    $('span.highlight').wrap("<span class='bg-success' style='font-weight:900'></span>")
+    // increases font-size for emphasis and provides highlighting of important lines
+    $('span.display-data').wrap("<span style='font-size:1.2em'></span>");
+    $('span.highlight').wrap("<span class='bg-success' style='font-weight:900'></span>");
 });
