5c06270db9dcdb15d03c2656c27f9010798370e6
mozart reina <mozart@mozartreina.com>
2015-11-25 21:01:01 +0530

    add post on Spacemacs and Rails

--- scripts/application.js
+++ scripts/application.js
@@ -3,4 +3,11 @@ $(document).ready(function (){
     // increases font-size for emphasis and provides highlighting of important lines
     $('span.display-data').wrap("<span style='font-size:1.2em'></span>");
     $('span.highlight').wrap("<span class='bg-success' style='font-weight:900'></span>");
+    // add x axis scrolling to code blocks
+    $('code').addClass('pre-x-scrollable');
+    // table styling for bootstrap
+    $('table').wrap("<div class='table-responsive'></div>");
+    $('table').addClass('table table-striped');
+    // console styling
+    // $('pre:has(code.language-console)').css({"background-color":"black", "color":"white"});
 });
